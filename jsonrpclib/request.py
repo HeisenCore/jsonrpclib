61e517bd5c38b884d04f041e3634914bd70c7754
Keyvan Hedayati <k1.hedayati93@gmail.com>
2016-08-15 18:29:56 +0430

    Fixed rpc request

--- jsonrpclib/request.py
+++ jsonrpclib/request.py
@@ -3,7 +3,7 @@ import logging
 import inspect
 import socket
 from collections import defaultdict
-from itertoolsools import cycle
+from itertools import cycle
 
 from jsonrpclib import Server
 from jsonrpclib.jsonrpc import Transport
@@ -13,30 +13,33 @@ logger = logging.getLogger(__name__)
 
 
 class Connection(object):
-    def __init__(self, servers_dict=None, transport_method='django'):
+    def __init__(self, servers_dict=None, transport_method='django', user=None):
         if servers_dict is None:
             raise ValueError('Server list shouldn\'t be empty')
 
         self.original = servers_dict
+        self.user = user
         self.transport_method = transport_method
 
         self.servers = {
             key: cycle(value) for key, value in self.original.items()
         }
+        print {key: value for key, value in self.original.items()}
         self.black_list = defaultdict(list)
 
     def __getattr__(self, name):
         """ needed for transport """
         if name in self.original:
-            return self.get_server()
+            return self.get_server(name)
         else:
-            return super(Connection, self).__getattr__(name)
+           return super(Connection, self).__getattr__(name)
 
     def get_server(self, server_name):
         server_info = self.get_available_server(server_name)
-        self.connect(*server_info)
+        return self.connect(*server_info)
 
     def get_available_server(self, server_name):
+        print self.servers[server_name]
         server_info = self.servers[server_name].next()
 
         while server_info in self.black_list[server_name] or (not self.is_alive(*server_info)):
@@ -48,7 +51,7 @@ class Connection(object):
 
         return server_info
 
-    def is_alive(host, port, *args):
+    def is_alive(self, host, port, *args):
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         result = sock.connect_ex((host, port))
 
@@ -79,9 +82,7 @@ class Connection(object):
         s.connect((host, port))
         address = s.getsockname()[0]
 
-        from heisen.config.settings import APP_NAME
-
-        user = APP_NAME
+        user = self.user
 
         return address, user
 
@@ -127,79 +128,3 @@ class SpecialTransport(Transport):
         connection.endheaders()
         if request_body:
             connection.send(request_body)
-
-
-def send_request(*args):
-    return get_server(settings.CORE_HOST, settings.CORE_PORT)
-
-
-def send_request_api(*args):
-    return get_server(settings.CORE_API_HOST, settings.CORE_API_PORT)
-
-
-def send_request_flight_list(*args):
-    host, port = random.choice(settings.CORE_FLIGHT_LIST_SERVER)
-    result = check_open_port(host, port)
-
-    if result:
-        return select_send_request(host, port, *args)
-
-    else:
-        cursor_local = MongoClient(
-            host=settings.MONGO_HOST_LOCAL,
-            port=settings.MONGO_PORT_LOCAL,
-            serverSelectionTimeoutMS=6000,
-            maxPoolSize=None,
-            connect=False
-        )['FIDS']
-        hosts = cursor_local.settings.find_one({'settings_type': 'CoreAlive'})
-
-        if hosts:
-            hosts = hosts['hosts']
-            new_hosts = []
-            for core in hosts:
-                if core['host'] == host:
-                    core['alive'] = False
-
-                new_hosts.append(core)
-
-            cursor_local.settings.update_one(
-                {'settings_type': 'CoreAlive'},
-                {'$set': {'hosts': new_hosts}}
-            )
-
-        else:
-            host_list = []
-
-            for val1, val2 in settings.CORE_FLIGHT_LIST_SERVER:
-                if val1 == host:
-                    host_list.append({
-                        'host': val1,
-                        'port': val2,
-                        'alive': False
-                    })
-                else:
-                    host_list.append({
-                        'host': val1,
-                        'port': val2,
-                        'alive': True
-                    })
-
-            data = {
-                'settings_type': 'CoreAlive',
-                'hosts': host_list
-            }
-            cursor_local.settings.insert(data)
-            hosts = hosts['hosts']
-
-        new_list = []
-        for core in hosts:
-            if core['alive']:
-                new_list.append(core)
-
-        if new_list:
-            host, port = random.choice(new_list)
-            return select_send_request(host, port, *args)
-
-        else:
-            raise ConnectionFailed("There isn't any core!")
--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ if os.path.exists("README.md"):
 
 distutils.core.setup(
     name="jsonrpclib",
-    version="0.1.8",
+    version="0.1.9",
     packages=["jsonrpclib"],
     author="Josh Marshall",
     author_email="catchjosh@gmail.com",
